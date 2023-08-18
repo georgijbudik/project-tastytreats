bd99f978951eff9b475643466ad75ddb3460fa17
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-08-18 23:53:02 +0300

    js-commit

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -4,28 +4,33 @@ const modalContent = document.querySelector('.modal-wrapper');
 const openModalBtn = document.querySelector('[data-action="open"]');
 const closeModalBtn = modal.querySelector('[data-action="close"]');
 
+openModalBtn.addEventListener('click', () => {
+  this.blur();
+  openModal();
+});
+closeModalBtn.addEventListener('click', closeModal);
+
+function outerClickHandler(e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+}
+
 function escapePressHandler(e) {
   if (e.code === 'Escape') {
     closeModal();
   }
 }
 
-openModalBtn.addEventListener('click', function () {
-  this.blur();
+function openModal() {
+  document.addEventListener('mousedown', outerClickHandler);
   document.addEventListener('keydown', escapePressHandler);
   modal.classList.remove('is-hidden');
   document.body.style.overflow = 'hidden';
-});
-
-closeModalBtn.addEventListener('click', closeModal);
-
-document.addEventListener('mousedown', function (event) {
-  if (event.target === modal) {
-    closeModal();
-  }
-});
+}
 
 function closeModal() {
+  document.removeEventListener('mousedown', outerClickHandler);
   document.removeEventListener('keydown', escapePressHandler);
   modal.classList.add('is-hidden');
   document.body.style.overflow = 'auto';
