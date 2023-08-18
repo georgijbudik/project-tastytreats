8490045f495399dd5eb0a9af12b1218a3e7794c6
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-08-18 22:04:02 +0300

    bugfix-commit

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -7,16 +7,31 @@ const closeModalBtn = modal.querySelector('[data-action="close"]');
 openModalBtn.addEventListener('click', function () {
   modal.classList.toggle('is-hidden');
   document.body.style.overflow = 'hidden';
+  window.addEventListener('keydown', escapePressHandler);
 });
 
 closeModalBtn.addEventListener('click', function () {
   modal.classList.toggle('is-hidden');
   document.body.style.overflow = 'auto';
+  window.removeEventListener('keydown', escapePressHandler);
 });
 
 window.addEventListener('mousedown', function (event) {
-  if (event.target === modal) {
-    modal.classList.toggle('is-hidden');
-    document.body.style.overflow = 'auto';
+  if (event.target !== modal) {
+    return;
   }
+
+  modal.classList.toggle('is-hidden');
+  document.body.style.overflow = 'auto';
+  window.removeEventListener('keydown', escapePressHandler);
 });
+
+function escapePressHandler(e) {
+  if (e.key !== 'Escape') {
+    return;
+  }
+
+  modal.classList.toggle('is-hidden');
+  window.removeEventListener('keydown', escapePressHandler);
+  document.body.style.overflow = 'auto';
+}
