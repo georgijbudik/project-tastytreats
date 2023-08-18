8277325bea61ab8ca24180d2c302a141763359f5
georgijbudik <georgijbudik@gmail.com>
2023-08-18 23:17:01 +0200

    start filters

--- src/js/api/gallery-api.js
+++ src/js/api/gallery-api.js
@@ -1,36 +1,39 @@
 import axios from 'axios';
 
-const BASE_URL = "https://tasty-treats-backend.p.goit.global/api/recipes"
-
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
 
 //------------Андрей ---------- //
-export const categorsCards = async (value,newPage,limit) => {
-  const response = await axios.get(`${BASE_URL}`,{params:{
-    page:newPage,
-    limit:limit,
-    category:value,
-  }});
+export const categorsCards = async (value, newPage, limit) => {
+  const response = await axios.get(`${BASE_URL}`, {
+    params: {
+      page: newPage,
+      limit: limit,
+      category: value,
+    },
+  });
   return response.data;
 };
 
-export const pageCards = async (newPage,limit) => {
-  const response = await axios.get(`${BASE_URL}`,{params:{
-    page:newPage,
-    limit:limit,
-  }});
+export const pageCards = async (newPage, limit) => {
+  const response = await axios.get(`${BASE_URL}`, {
+    params: {
+      page: newPage,
+      limit: limit,
+    },
+  });
   return response.data;
 };
 
 //--------------Гоша----------------------//
-export const filtrCards = async (page,area,time,id,limit) => {
-  const response = await axios.get(`${BASE_URL}`,{params:{
-    page:page,
-    area:area,
-    time:time,
-    id:id,
-    limit:limit,
-  }});
+export const filtrCards = async (page, area, time, id, limit) => {
+  const response = await axios.get(`${BASE_URL}`, {
+    params: {
+      page: page,
+      area: area,
+      time: time,
+      id: id,
+      limit: limit,
+    },
+  });
   return response.data;
 };
-
-
