38023c01456780f7b06e6f9f6af2c19985d75785
AndriiMishch <anderito02@gmail.com>
2023-08-19 14:02:21 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -58,3 +58,48 @@
 
 })();
 
+const ratingModal = document.querySelectorAll('.rating-modal')
+
+if(ratingModal.length > 0) {
+    initRatingsModal();
+}
+
+function initRatingsModal(){
+    let ratingActive, ratingValue;
+    for(let index = 0; index < ratingModal.length; index ++) {
+        const rating = ratingModal[index];
+        initRatingsModal(rating);
+    }
+
+    function initRatingsModal(rating) {
+        initRatingsModalVars(rating);
+
+        setRatingModalActiveWidth();
+
+        if (rating.classList.contains('rating__set')) {
+            setRatingModal(rating);
+        }
+    }
+
+    function initRatingsModalVars(rating) {
+        ratingActive = rating.querySelector('.rating__active')
+        ratingValue = rating.querySelector('.rating__value')
+    }
+
+    function setRatingModalActiveWidth (index = ratingValue.innerHTML) {
+        const ratingAktiveWidth = index/0.05;
+        ratingActive.style.width = `${ratingAktiveWidth}%`;
+    }
+
+    function setRatingModal(rating) {
+        const ratingItemsModal = rating.querySelector('.rating__item');
+        for (let index = 0; index < ratingItemsModal.length; index++) {
+            const ratingItemModal = ratingItemsModal[index];
+            ratingItemModal.addEventListener("mouseenter", function (e) {
+                initRatingsModalVars(rating);
+
+                setRatingModalActiveWidth(ratingItemModal.value)
+            });
+        }
+    }
+}
