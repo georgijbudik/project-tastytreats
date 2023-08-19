e8b1af6eb6a7d5f43b7fcdf9ecce07187189c196
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-08-19 15:05:48 +0300

    darkTheme-firstCommit

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './js/pop-up';
 import './js/form';
 import './js/api/popular-api';
 import './js/categories';
-import "./js/renderCards"
+import "./js/renderCards";
 import './js/modal';
+import './js/changeTheme';
