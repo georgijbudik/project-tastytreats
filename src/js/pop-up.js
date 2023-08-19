fdb3d8a0d2f4dff4596d47fcb14229b3e0a4a6cc
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-08-19 14:30:45 +0300

    adaptive-commit

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -4,8 +4,8 @@ const modalContent = document.querySelector('.modal-wrapper');
 const openModalBtn = document.querySelector('[data-action="open"]');
 const closeModalBtn = modal.querySelector('[data-action="close"]');
 
-openModalBtn.addEventListener('click', () => {
-  this.blur();
+openModalBtn.addEventListener('click', e => {
+  e.currentTarget.blur();
   openModal();
 });
 closeModalBtn.addEventListener('click', closeModal);
@@ -23,15 +23,15 @@ function escapePressHandler(e) {
 }
 
 function openModal() {
-  document.addEventListener('mousedown', outerClickHandler);
-  document.addEventListener('keydown', escapePressHandler);
+  window.addEventListener('mousedown', outerClickHandler);
+  window.addEventListener('keydown', escapePressHandler);
   modal.classList.remove('is-hidden');
   document.body.style.overflow = 'hidden';
 }
 
 function closeModal() {
-  document.removeEventListener('mousedown', outerClickHandler);
-  document.removeEventListener('keydown', escapePressHandler);
+  window.removeEventListener('mousedown', outerClickHandler);
+  window.removeEventListener('keydown', escapePressHandler);
   modal.classList.add('is-hidden');
   document.body.style.overflow = 'auto';
 }
