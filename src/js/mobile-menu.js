afe6f1b4b7cb9289ce88e530179610bce651dcfb
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-08-20 17:37:56 +0300

    Fix

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,14 @@
-const mobMenuBtn = document.querySelector('.mobile-menu-btn');
+const mobMenuBtnOpen = document.querySelector('.mobile-menu-btn');
 const mobMenuCloseBtn = document.querySelector('.mobile-menu-btn-close');
 const mobileMenu = document.querySelector('.mobile-menu');
+const mobileBodyScroll = document.body; 
 
-mobMenuBtn.addEventListener('click', function() {
-  mobileMenu.classList.toggle('open');
+mobMenuBtnOpen.addEventListener('click', function() {
+  mobileMenu.classList.add('open');
+  mobileBodyScroll.classList.add('no-scroll')  
 });
 
 mobMenuCloseBtn.addEventListener('click', function() {
   mobileMenu.classList.remove('open');
+  mobileBodyScroll.classList.remove('no-scroll')
 });
