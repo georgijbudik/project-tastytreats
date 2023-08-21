978fb5006b4163670eb56f4eaa7e058dfe5f4e17
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-08-21 15:55:50 +0300

    commit

--- src/js/api/recipe-info-api.js
+++ src/js/api/recipe-info-api.js
@@ -1,117 +1,13 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
-// const ID = '6462a8f74c3d0ddd28898040';
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes/';
 
-// export default async function renderModal() {
-//   try {
-//     const recipe = await getRecipeDataById(ID);
-//     console.log(recipe);
-//     renderModalByRecipe(recipe.data[0]);
-//   } catch (error) {
-//     console.warn(error.message);
-//   }
-// }
-
-// function getRecipeDataById(id) {
-//   return axios.get(`${BASE_URL}/recipes/${id}`);
-// }
-
-// function renderModalByRecipe(recipe) {
-//   const markup = `
-//     <div class="modal-wrapper" id="myModal">
-//       <button class="close-modal-button" data-action="close">
-//         <svg class="close-modal-button-icon">
-//           <use href="./images/sprite/sprite.svg#icon-cross"></use>
-//         </svg>
-//       </button>
-
-//       <div class="modal-recipe-name-and-img">
-//         <h2 class="modal-recipe-name">${recipe.title}</h2>
-//         <img class="modal-recipe-img" src="${recipe.preview}" alt="${
-//     recipe.title
-//   }" style="background: linear-gradient(
-//       0deg,
-//       rgba(5, 5, 5, 0.4) 0%,
-//       rgba(5, 5, 5, 0.4) 100%
-//     ),
-//     url('${recipe.thumb}'), lightgray -34.64px -20px / 109.993% 120%;"/>
-//       </div>
-
-//       <div class="modal-recipe-info">
-//         <ul class="modal-recipe-info-tag-list">
-//         ${recipe.tags.map(tag => {
-//           return `<li class="modal-recipe-info-tag-item">#${tag}</li>`;
-//         })}
-//         </ul>
-//         <div class="modal-recipe-info-rating">
-//           <p class="modal-recipe-info-rating-points">${recipe.rating}</p>
-//           <ul class="modal-recipe-info-rating-stars-list">
-//             <li class="modal-recipe-info-rating-star">
-//               <svg class="modal-recipe-info-rating-star-svg">
-//                 <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
-//               </svg>
-//             </li>
-//             <li class="modal-recipe-info-rating-star">
-//               <svg class="modal-recipe-info-rating-star-svg">
-//                 <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
-//               </svg>
-//             </li>
-//             <li class="modal-recipe-info-rating-star">
-//               <svg class="modal-recipe-info-rating-star-svg">
-//                 <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
-//               </svg>
-//             </li>
-//             <li class="modal-recipe-info-rating-star">
-//               <svg class="modal-recipe-info-rating-star-svg">
-//                 <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
-//               </svg>
-//             </li>
-//             <li class="modal-recipe-info-rating-star">
-//               <svg class="modal-recipe-info-rating-star-svg">
-//                 <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
-//               </svg>
-//             </li>
-//           </ul>
-//           <p class="modal-recipe-info-cooking-time">${recipe.time} min</p>
-//         </div>
-//       </div>
-
-//       <ul class="modal-recipe-ingredient-list">
-//       ${recipe.ingredients
-//         .map(ingredient => {
-//           return `
-//         <li class="modal-recipe-ingredient-item">
-//         <p class="modal-recipe-ingredient-item-name">${ingredient.name}</p>
-//         <p class="modal-recipe-ingredient-item-quantity">${ingredient.measure}</p>
-//         </li>`;
-//         })
-//         .join('')}
-//       </ul>
-
-//       <p class="modal-recipe-cooking-description">
-//       ${recipe.instructions}
-//       </p>
-
-//       <ul class="modal-recipe-button-list">
-//         <li class="modal-recipe-button-item">
-//           <button
-//             type="button"
-//             class="modal-recipe-add-button modal-recipe-button"
-//           >
-//             Add to favorite
-//           </button>
-//         </li>
-//         <li class="modal-recipe-button-item">
-//           <button
-//             data-rating-open
-//             type="button"
-//             class="modal-recipe-rating-button modal-recipe-button"
-//           >
-//             Give rating
-//           </button>
-//         </li>
-//       </ul>
-//     </div>`;
-//   backdrop.innerHTML = markup;
-// }
+export default async function fetchRecipeById(id) {
+  try {
+    const recipeResponse = await axios.get(`${BASE_URL}${id}`);
+    const recipe = recipeResponse.data;
+    return recipe;
+  } catch (error) {
+    console.warn(error.message);
+  }
+}
--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,167 +1,18 @@
-const exmpl = {
-  _id: '6462a8f74c3d0ddd288980d4',
-  title: 'Borscht',
-  category: 'Soup',
-  area: 'Ukrainian',
-  instructions:
-    'Preheat the oven to 200C. In an ovenproof dish, bake the pork ribs for about 30 minutes or until a golden crust forms. Meanwhile, wash and roughly chop the celery root. There is no need to peel it. Cut up the carrots into chunks of about the same size as the celery pieces. Transfer the baked ribs into a large pot, add about 3 litres of water. Add the celery and carrot chunks, and half an unpeeled onion. Bring to a boil and cook over medium heat for about 30 minutes.\r\n\r\nMake the sauteed vegetable base – this is what makes any borscht so flavorful. Dice the sweet bell pepper. Finely dice the tomatoes and the other half of the onion.\r\n\r\nHeat the butter in a frying pan and cook the vegetables until they soften. Add 200ml tomato juice and about 2 tbsp tomato paste and cook for another 5-7 minutes.\r\n\r\nUsing a box grater, shred one of the beetroots. Add it to the sauteed vegetables and keep cooking for another 3-4 minutes.\r\n\r\nUsing a food processor with a juicing attachment, squeeze the juice out of the other beet (alternatively, you can simply grate it using the fine attachment on your box grater). Add the beet juice or purée and the vegetables into the pot with the ribs and celery, along with the allspice and bay leaves. Salt to taste.\r\n\r\nWash and peel 3-4 potatoes.\r\n\r\nDice them roughly and add into the pot with the rib broth. Cut the head of garlic in half and add to the pot.\r\n\r\nShred the cabbage and set it aside – we will add it to the borscht almost at the very end.\r\n\r\nIf you like, you can add some canned white beans to the borscht at this point.\r\n\r\nAdd the smoked dried pears to the pot. They will enhance the borscht with their wonderful smoky flavor.\r\n\r\nOnce all of the ingredients have cooked through and are tender, add the shredded cabbage to the pot. Cook for 5 more minutes and take the borscht off the heat. Let it rest for at least half an hour before serving with sour cream and finely chopped dill. Keep the borscht in the fridge and remember that it will become even more delicious overnight.',
-  description:
-    'It’s no secret that almost every family has its own special borscht recipe. Some of us just can’t imagine it without beans, while others prefer to cook it without cabbage. All these options have a right to exist, because there is no single “correct” recipe, just hundreds of fascinating variations.The Ukrainian soul lives in this dish.',
-  thumb:
-    'https://klopotenko.com/wp-content/uploads/2021/11/ukrainian-borscht-with-pork-ribs_sitewebeng-1000x600.jpg?v=1637229883',
-  preview:
-    'https://klopotenko.com/wp-content/uploads/2021/11/ukrainian-borscht-with-pork-ribs_sitewebeng-630x517.jpg?v=1637229883',
-  time: '90',
-  youtube: 'https://youtu.be/qkTlcfCw7jQ',
-  tags: ['Soup'],
-  ingredients: [
-    {
-      id: '640c2dd963a319ea671e366d',
-      measure: '400-500 g',
-      name: 'Beef Fillet',
-      desc: 'Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times. Beef is a source of high-quality protein and nutrients.\r\n\r\nMost beef skeletal muscle meat can be used as is by merely cutting into certain parts, such as roasts, short ribs or steak (filet mignon, sirloin steak, rump steak, rib steak, rib eye steak, hanger steak, etc.), while other cuts are processed (corned beef or beef jerky). Trimmings, on the other hand, are usually mixed with meat from older, leaner (therefore tougher) cattle, are ground, minced or used in sausages. The blood is used in some varieties called blood sausage. Other parts that are eaten include other muscles and offal, such as the oxtail, liver, tongue, tripe from the reticulum or rumen, glands (particularly the pancreas and thymus, referred to as sweetbread), the heart, the brain (although forbidden where there is a danger of bovine spongiform encephalopathy, BSE, commonly referred to as mad cow disease), the kidneys, and the tender testicles of the bull (known in the United States as calf fries, prairie oysters, or Rocky Mountain oysters). Some intestines are cooked and eaten as is, but are more often cleaned and used as natural sausage casings. The bones are used for making beef stock.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564216/zuhii6vl0wgaybsgjexk.png',
-    },
-    {
-      id: '640c2dd963a319ea671e3895',
-      measure: '2',
-      name: 'Beetroot',
-      desc: 'The beetroot is the taproot portion of a beet plant, usually known in Canada and the USA as beets while the vegetable is referred to as beetroot in British English, and also known as the table beet, garden beet, red beet, dinner beet or golden beet.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678565178/xnjeqypnaoqwx5ik4p1k.png',
-    },
-    {
-      id: '640c2dd963a319ea671e377f',
-      measure: '2',
-      name: 'Tomatoes',
-      desc: 'A juicy and flavorful fruit that comes in a variety of colors and sizes, commonly used in many cuisines around the world, particularly in Mediterranean and Latin American cuisines.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564915/ffdbunsbgbsqysaezexw.png',
-    },
-    {
-      id: '640c2dd963a319ea671e368a',
-      measure: '1/4',
-      name: 'Celery',
-      desc: 'A long, crunchy stalk vegetable with a green color and a strong, distinct flavor, often used in soups, stews, salads, and as a snack with dips or spreads.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564674/hxotah2bhhhmxmj9znvu.png',
-    },
-    {
-      id: '640c2dd963a319ea671e37f5',
-      measure: '1/4 head',
-      name: 'Cabbage',
-      desc: 'A leafy green or purple vegetable that is often used in salads, coleslaw, and stir-fry dishes, and is also commonly fermented into sauerkraut.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564979/jm8ucv9yfwvdy8bxtfjt.png',
-    },
-    {
-      id: '640c2dd963a319ea671e3746',
-      measure: '3-4',
-      name: 'Potatoes',
-      desc: 'A starchy root vegetable that is commonly used in cooking and is a good source of carbohydrates, fiber, and vitamins.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564915/dxeipz28barkkn55fj7y.png',
-    },
-    {
-      id: '640c2dd963a319ea671e3684',
-      measure: '1-2',
-      name: 'Carrots',
-      desc: 'A root vegetable that is commonly used in cooking and baking, with a sweet and earthy flavor, and high in vitamin A and other nutrients.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564673/aoyitr2lgxl65uxtt3dn.png',
-    },
-    {
-      id: '640c2dd963a319ea671e369f',
-      measure: '1',
-      name: 'Chopped Onion',
-      desc: 'Pieces of onion that have been diced or chopped, commonly used as a flavor base for soups, stews, and other dishes.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564742/vbloj8qjqlezkviowjez.png',
-    },
-    {
-      id: '640c2dd963a319ea671e382c',
-      measure: '1-2 smoked dried',
-      name: 'Pears',
-      desc: 'A sweet and juicy fruit with a soft, grainy texture and thin skin.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564979/njafxdc9oaowdrjrgnf8.png',
-    },
-    {
-      id: '640c2dd963a319ea671e366b',
-      measure: '2',
-      name: 'Bay Leaves',
-      desc: 'The bay leaf is an aromatic leaf commonly used in cooking. It can be used whole, or as dried and ground.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564216/oucufhpi7o8xtvliebgs.png',
-    },
-    {
-      id: '640c2dd963a319ea671e3860',
-      measure: '3 cloves',
-      name: 'Allspice',
-      desc: 'A spice made from the dried berries of the Pimenta dioica plant, with a flavor profile that is a combination of cinnamon, nutmeg, and cloves, commonly used in Caribbean and Latin American cuisine.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678565047/t8u4ogpqcghpzuiddoxl.png',
-    },
-    {
-      id: '640c2dd963a319ea671e36e4',
-      measure: '1 head',
-      name: 'Garlic Clove',
-      desc: 'A small, bulbous vegetable consisting of several smaller cloves with a pungent flavor commonly used in cooking.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564798/q7joqmqmoxxnnalupvmq.png',
-    },
-    {
-      id: '640c2dd963a319ea671e37e5',
-      measure: '1',
-      name: 'Yellow Pepper',
-      desc: 'Yellow peppers are a type of bell pepper with a sweet, mild flavor. They are commonly used in salads, stir-fries, and other culinary applications.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564978/fyi5lre9cfsg0dv2w1az.png',
-    },
-    {
-      id: '640c2dd963a319ea671e377e',
-      measure: '200 ml',
-      name: 'Tomato Puree',
-      desc: 'A thick, concentrated paste made from cooked and strained tomatoes, used as a base for sauces, soups, and stews.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564915/h4rdnf9unpjvlsiexzvx.png',
-    },
-    {
-      id: '640c2dd963a319ea671e3828',
-      measure: '30 g',
-      name: 'Unsalted Butter',
-      desc: 'Butter made from cream that has been separated from milk and churned, without any added salt',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564978/n6um1o4paeognjd0sors.png',
-    },
-    {
-      id: '640c2dd963a319ea671e37ba',
-      measure: '2 tbsp',
-      name: 'Passata',
-      desc: 'Tomato puree made from strained and cooked tomatoes, commonly used as a base for pasta sauces.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564914/auyjzjg2lww1sttrfxb5.png',
-    },
-    {
-      id: '640c2dd963a319ea671e375e',
-      measure: '',
-      name: 'Salt',
-      desc: 'Crystalline mineral made of sodium chloride, commonly used as a seasoning and preservative in cooking and food preparation.',
-      img: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564910/a1sigxrvvbfhsrozsshz.png',
-    },
-  ],
-  rating: 2.55,
-  whoRated: 1,
-};
-
-// import axios from 'axios';
-// const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
-// const ID = '6462a8f74c3d0ddd28898040';
+import fetchRecipeById from './api/recipe-info-api';
 
 const backdrop = document.querySelector('.popup-backdrop');
-
-// function getRecipeDataById(id) {
-//   return axios.get(`${BASE_URL}/recipes/${id}`);
-// }
-
-// getRecipeDataById(ID).then(console.log);
-
 const modalRecipe = document.querySelector('.modal-recipe');
 
-renderModalByRecipe(exmpl);
-
-const openModalBtn = document.querySelector('[data-action="open"]');
+const openModalBtn = document.querySelectorAll('[data-action="open"]');
 const closeModalBtn = document.querySelector('[data-action="close"]');
 
-openModalBtn.addEventListener('click', e => {
-  e.currentTarget.blur();
-  openModal();
+openModalBtn.forEach(btn => {
+  btn.addEventListener('click', e => {
+    e.currentTarget.blur();
+    openModal();
+  });
 });
+
 closeModalBtn.addEventListener('click', closeModal);
 
 function outerClickHandler(e) {
@@ -190,10 +41,10 @@ function closeModal() {
   document.body.style.overflow = 'auto';
 }
 
+fetchRecipeById('6462a8f74c3d0ddd28898056').then(renderModalByRecipe);
+
 function renderModalByRecipe(recipe) {
   const markup = `
-
-
       <div class="modal-recipe-name-and-img">
         <h2 class="modal-recipe-name">${recipe.title}</h2>
         <img class="modal-recipe-img" src="${recipe.preview}" alt="${
@@ -208,36 +59,38 @@ function renderModalByRecipe(recipe) {
 
       <div class="modal-recipe-info">
         <ul class="modal-recipe-info-tag-list">
-        ${recipe.tags.map(tag => {
-          return `<li class="modal-recipe-info-tag-item">#${tag}</li>`;
-        })}
+        ${recipe.tags
+          .map(tag => {
+            return `<li class="modal-recipe-info-tag-item">#${tag}</li>`;
+          })
+          .join('')}
         </ul>
         <div class="modal-recipe-info-rating">
           <p class="modal-recipe-info-rating-points">${recipe.rating}</p>
           <ul class="modal-recipe-info-rating-stars-list">
             <li class="modal-recipe-info-rating-star">
-              <svg class="modal-recipe-info-rating-star-svg">
-                <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
+              <svg class="modal-recipe-info-rating-star-svg" viewBox="0 0 32 32">
+                <path d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
               </svg>
             </li>
             <li class="modal-recipe-info-rating-star">
-              <svg class="modal-recipe-info-rating-star-svg">
-                <use href=./images/sprite/sprite.svg#icon-star-rating></use>
+              <svg class="modal-recipe-info-rating-star-svg" viewBox="0 0 32 32">
+                <path d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
               </svg>
             </li>
             <li class="modal-recipe-info-rating-star">
-              <svg class="modal-recipe-info-rating-star-svg">
-                <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
+              <svg class="modal-recipe-info-rating-star-svg" viewBox="0 0 32 32">
+                <path d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
               </svg>
             </li>
             <li class="modal-recipe-info-rating-star">
-              <svg class="modal-recipe-info-rating-star-svg">
-                <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
+              <svg class="modal-recipe-info-rating-star-svg" viewBox="0 0 32 32">
+                <path d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
               </svg>
             </li>
             <li class="modal-recipe-info-rating-star">
-              <svg class="modal-recipe-info-rating-star-svg">
-                <use href="./images/sprite/sprite.svg#icon-star-rating"></use>
+              <svg class="modal-recipe-info-rating-star-svg" viewBox="0 0 32 32">
+                <path d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
               </svg>
             </li>
           </ul>
