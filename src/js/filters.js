fad543067a007c52744873996ff37189f4fbd5bc
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-08-22 00:13:02 +0200

    update

--- src/js/filters.js
+++ src/js/filters.js
@@ -209,43 +209,7 @@ const handleSelectArea = () => {
       });
   }
 };
-// const createRecipeCard = recipe => {
-//   return `<li>
-//                 <div class="icon-heart">
-//                 <a>
-//               <svg height="22px" id="icon-heart" viewBox="0 0 36 32">
-//               <path class="svg" fill="none" opacity="0.8" stroke="#f8f8f8"  stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.9091" d="M15.991 6.848c-2.666-3.117-7.113-3.956-10.451-1.101-3.341 2.854-3.811 7.625-1.188 11.001 2.182 2.806 8.781 8.724 10.944 10.64 0.243 0.214 0.364 0.321 0.505 0.364 0.057 0.017 0.123 0.028 0.191 0.028s0.133-0.010 0.195-0.029l-0.005 0.001c0.141-0.042 0.262-0.15 0.505-0.364 2.163-1.916 8.762-7.834 10.943-10.64 2.624-3.375 2.211-8.177-1.187-11.001-3.398-2.825-7.785-2.016-10.451 1.101z"></path>
-//               </svg>
-//               </a>
-//                 </div>
-//                 <img
-//                   src="${recipe.preview}"
-//                   alt=""
-//                   width="335px"
-//                   height="335px"
-//                 />
-//                 <div class="description-rating-container">
-//                   <h3>${recipe.title}</h3>
-//                   <p>${recipe.description}a</p
-//                   >
-//                   <div class="rating">
-//                   <div>
-//                     <span class="number">${recipe.rating}</span>
-//                       <ul class="rating-stars-list">
-//                       </ul>
-//                       </div>
-//                       <button class="see-recipe-button" type="button">
-//                         See recipe
-//                       </button>
-//                   </div>
-//                 </div>
-//               </li>`;
-// };
 
-// const renderCards = recipes => {
-//   const markup = recipes.map(createRecipeCard).join('');
-//   refs.galleryListEl.innerHTML = markup;
-// };
 
 const handleSelectIngredients = () => {
   let page = 1;
@@ -320,24 +284,6 @@ const handleSelectIngredients = () => {
       });
   }
 
-  // // filterFood(ingredient, limit, page)
-  //   .then(data => {
-  //     if (data.results.length === 0) {
-  //       clearGallery();
-  //       return render();
-  //     }
-  //
-  //     clearGallery();
-  //     createMarkup(data.results);
-  //     const jsSeeRecipeBtnRef = document.querySelectorAll('.js-see-recipe');
-  //     jsSeeRecipeBtnRef.forEach(btn => {
-  //       btn.addEventListener('click', e => {
-  //         clickModal = e.target.dataset.id;
-  //         openModal(clickModal);
-  //       });
-  //     });
-  //   })
-  //   .catch();
 };
 const debouncedOnChange = debounce(handleInput, 300);
 
@@ -372,39 +318,10 @@ const renderIngredients = ingredients => {
   refs.selectFoodEl.insertAdjacentHTML('beforeend', optionsHTML);
 };
 
-// selectTime().then(response => {
-//   console.log(response);
-//   renderTime(response);
-// });
-
-// const renderTime = time => {
-//   const optionsHTML = time
-//     .map(
-//       option =>
-//         `<option value="${option._id}" class="select-option">${option.time} min</option>`
-//     )
-//     .join('');
-//   refs.selectTimeEl.insertAdjacentHTML('beforeend', optionsHTML);
-// };
-
-// refs.selectTimeEl.addEventListener('change', renderTime);
-
-// const renderArea = areas => {
-//   const optionsHTML = areas
-//     .map(
-//       area =>
-//         `<option value="${option._id}" class="select-option">${option.time} min</option>`
-//     )
-//     .join('');
-//   refs.selectTimeEl.insertAdjacentHTML('beforeend', optionsHTML);
-// };
-
-// refs.selectTimeEl.addEventListener('change', renderArea);
 
 const handleResetFilters = () => {
   let page = 1;
   let limit = 6;
-  // const totalItems = data.results.length * data.totalPages;
 
   refs.filterInputEl.value = '';
   refs.selectTimeEl.value = 'time';
