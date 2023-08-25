1ac4b06035aa133df7db40c12eb53650326c7420
georgijbudik <georgijbudik@gmail.com>
2023-08-25 15:33:33 +0200

    fix

--- src/js/activePage.js
+++ src/js/activePage.js
@@ -1,11 +1,18 @@
 const currentPage = window.location.href;
 
 function setActiveClass(elements) {
+  let isActiveSet = false;
   elements.forEach(function (element) {
     if (element.href === currentPage) {
       element.classList.add('is-active');
+      isActiveSet = true;
+    } else {
+      element.classList.remove('is-active');
     }
   });
+  if (!isActiveSet) {
+    elements[0].classList.add('is-active'); // Встановлюємо активне посилання на перший елемент (Home)
+  }
 }
 
 const activePage = document.querySelectorAll('nav a');
